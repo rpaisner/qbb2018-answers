75d04f3215868d933a21e48dea8caf7454130c49
Rebekka Paisner <rpaisne2@jhu.edu>
2018-08-28 19:58:17 -0400

    More comments, this time with explanation of the third argument

--- day2-homework/fly_appendingAC.py
+++ day2-homework/fly_appendingAC.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" I am going to make my fly_data from the parsed version of fly.txt and my t_data from the ctab file we were working on in class"""
+""" I am going to make my fly_data from the parsed version of fly.txt and my t_data from the ctab file we were working on in class. The third argument is to let us know where there were no matchs."""
 import sys
 fly_data = open( sys.argv[1] )
 t_data = open( sys.argv[2] )
@@ -9,9 +9,9 @@ for line in fly_data:
     fields = line.rstrip("\r\n").split()
     fly_dict[fields[0]] = fields[1]
 
+count = 0
 for line2 in t_data:
     line2=line2.strip()
-    count = 0
     if "FBgn" in line2:
         fields2 = line2.rstrip("\r\n").split()
         if fields2[8] in fly_dict.keys():
